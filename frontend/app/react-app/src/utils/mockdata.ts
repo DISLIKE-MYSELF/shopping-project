3210f98f686e9a6ca5394893aea05ad6f5aa527b
chakiri <nijine.chakiri@gmail.com>
2025-06-04 13:18:11 +0800

    增加更多商品

--- frontend/app/react-app/src/utils/mockdata.ts
+++ frontend/app/react-app/src/utils/mockdata.ts
@@ -9,31 +9,116 @@ const instance = axios.create({
 
 const mock = new MockAdapter(instance, { delayResponse: 1000 })
 
-const mockProduct1: Product = {
-  id: 1,
-  name: '智能手表',
-  price: 199.99,
-  description: '这是一个智能手表。',
-  image: 'watch.png',
-  category: '电子产品',
-  stock: 5,
-  rating: 4.5,
-  createAt: '2024-06-01T12:00:00Z',
-  updateAt: '2024-06-10T12:00:00Z',
-}
-
-const mockProduct2: Product = {
-  id: 2,
-  name: '耳机',
-  price: 299.99,
-  description: '这是一个耳机。',
-  image: 'headphones.png',
-  category: '电子产品',
-  stock: 3,
-  rating: 3.5,
-  createAt: '2024-06-01T12:00:00Z',
-  updateAt: '2024-06-10T12:00:00Z',
-}
+const mockProducts: Product[] = [
+  {
+    id: 1,
+    name: '智能手表',
+    price: 599,
+    description: '小米智能手表。',
+    image: 'watch.jpg',
+    category: '电子产品',
+    stock: 5,
+    rating: 4.5,
+    createAt: '2024-06-01T12:00:00Z',
+    updateAt: '2024-06-10T12:00:00Z',
+  },
+  {
+    id: 2,
+    name: '耳机',
+    price: 1999,
+    description: 'AKG K-701',
+    image: 'headphones.png',
+    category: '电子产品',
+    stock: 3,
+    rating: 4.5,
+    createAt: '2024-06-01T12:00:00Z',
+    updateAt: '2024-06-10T12:00:00Z',
+  },
+  {
+    id: 3,
+    name: '小米15',
+    price: 4199,
+    description: '小米15。',
+    image: 'xiaomi15.png',
+    category: '电子产品',
+    stock: 3,
+    rating: 3.5,
+    createAt: '2024-06-01T12:00:00Z',
+    updateAt: '2024-06-10T12:00:00Z',
+  },
+  {
+    id: 4,
+    name: 'iphone16',
+    price: 6999,
+    description: 'iphone16。',
+    image: 'iphone.png',
+    category: '电子产品',
+    stock: 3,
+    rating: 3.5,
+    createAt: '2024-06-01T12:00:00Z',
+    updateAt: '2024-06-10T12:00:00Z',
+  },
+  {
+    id: 5,
+    name: 'HUAWEI Mate XT',
+    price: 28300,
+    description: '怎么折都有面。',
+    image: 'huaweiFoldPhone.jpg',
+    category: '电子产品',
+    stock: 3,
+    rating: 3.5,
+    createAt: '2024-06-01T12:00:00Z',
+    updateAt: '2024-06-10T12:00:00Z',
+  },
+  {
+    id: 6,
+    name: '华为MateBook Fold',
+    price: 29999,
+    description: '怎么折都有面。',
+    image: 'huaweiFoldComputer.jpg',
+    category: '电子产品',
+    stock: 3,
+    rating: 3.5,
+    createAt: '2024-06-01T12:00:00Z',
+    updateAt: '2024-06-10T12:00:00Z',
+  },
+  {
+    id: 7,
+    name: '奶龙',
+    price: 114514,
+    description: '我才是奶龙！',
+    image: 'nailong.png',
+    category: '???',
+    stock: 1,
+    rating: 3.5,
+    createAt: '2024-06-01T12:00:00Z',
+    updateAt: '2024-06-10T12:00:00Z',
+  },
+  {
+    id: 8,
+    name: '丰川祥子',
+    price: 99999999,
+    description: '哇这个好可爱啊desuwa',
+    image: 'saki.jpg',
+    category: '???',
+    stock: 1,
+    rating: 3.5,
+    createAt: '2024-06-01T12:00:00Z',
+    updateAt: '2024-06-10T12:00:00Z',
+  },
+  {
+    id: 9,
+    name: '冬雪莲',
+    price: 99999999,
+    description: '非常稀有的商品。',
+    image: 'rare.png',
+    category: '???',
+    stock: 0,
+    rating: 3.5,
+    createAt: '2024-06-01T12:00:00Z',
+    updateAt: '2024-06-10T12:00:00Z',
+  },
+]
 
 const mockCartItem1: CartItem = {
   id: 1,
@@ -49,22 +134,11 @@ const mockCartItem2: CartItem = {
   createAt: '2024-06-01T12:00:00Z',
 }
 
-mock.onGet('/products/1').reply(200, mockProduct1)
-mock.onGet('/products/2').reply(200, mockProduct2)
-mock.onGet('/products/3').reply(404, mockProduct2)
+mockProducts.map((product) => {
+  mock.onGet(`/products/${product.id}`).reply(200, product)
+})
 
-mock
-  .onGet('/products')
-  .reply(200, [
-    mockProduct1,
-    mockProduct2,
-    mockProduct1,
-    mockProduct2,
-    mockProduct1,
-    mockProduct2,
-    mockProduct1,
-    mockProduct2,
-  ])
+mock.onGet('/products').reply(200, mockProducts)
 
 mock.onGet('/carts/user/1').reply(200, [mockCartItem1, mockCartItem2])
 
